a9dbb04e296adff40a7691446c956c5460509ac4
dreadheaddreamz <github email address>
2021-06-14 13:33:10 -0500

    added the add button feature from tut

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-
+import React,{useState} from 'react'
 import './App.css';
 import InputArea from './components/InputArea';
+import ToDoItem from './components/ToDoItem'
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const addItems = (inputText) => {
+    setItems((prevItems) => {
+      return [...prevItems,inputText]
+    })
+  }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <InputArea />
+      <InputArea onAdd={addItems} />
+      <div>
+        <ul>
+        {items.map((item, index) => (<ToDoItem key={index} id={index} text={item}/>
+      ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-const InputArea = () => {
+const InputArea = (props) => {
+    const [inputText, setInputText] = useState('');
+
+    const handleChange = (event) => {
+        const newValue = event.target.value
+        setInputText(newValue)
+    }
     return(
         <div className="form">
-            <input type="text"/>
-            <button><span>Add</span></button>
+            <input type="text" onChange={handleChange} value={inputText} />
+            <button onClick={() => { 
+                props.onAdd(inputText)
+                setInputText('') //
+            }}><span>Add</span></button>
         </div>
     )
 }
